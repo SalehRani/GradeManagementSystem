bbfeb7ed5501b467a35f48fa4def17d3c18c43de
Rani Saleh <xx.rani.saleh.xx@hotmail.com>
2024-01-09 01:08:07 -0500

    Setup basic layout of GradeManagementSystem

---
+++ src/main/java/CourseGradeSemesterCLI.java
@@ -0,0 +1,5 @@
+public class CourseGradeSemesterCLI {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/dao/JdbcSemesterDao.java
@@ -0,0 +1,4 @@
+package dao;
+
+public class JdbcSemesterDao implements SemesterDao {
+}
